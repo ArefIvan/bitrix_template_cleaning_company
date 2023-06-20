95fe26f814b36f98958cdc715d4f3f01260fa1e7
aref-ivan <aref.ivan@gmail.com>
2023-06-20 16:23:03 +0300

    start project

---
+++ README.md
@@ -0,0 +1,3 @@
+# bitrix_template_cleaning_company
+
+Шаблон для сайта клининговой компании. Для 1c-bitrix.
---
+++ local/templates/CleaningCompany/description.php
@@ -0,0 +1,9 @@
+<?
+if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
+	die();
+
+$arTemplate = Array(
+	'NAME' => 'CleaningCompany',
+	'DESCRIPTION' => 'Шаблон сайта клининговой компании1'
+);
+?>
